5fab422c8be658755622eab128061fc12ea9f76a
Daun Kim <yennakim11@gmail.com>
2024-07-01 00:11:08 -0500

    implemented CRUD

--- components/EditTodoForm.js
+++ components/EditTodoForm.js
@@ -1,7 +1,22 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
-const EditTodoForm = () => {
-  <div>EditTodoForm</div>;
+const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) => {
+    // prevent default action
+    e.preventDefault();
+    // edit todo
+    editTodo(value, task.id);
+  };
+  return (
+    <Form onSubmit={handleSubmit} className="TodoForm">
+      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder="Update task" />
+      <Button variant="primary" type="submit" className="todo-btn">Update task</Button>
+    </Form>
+  );
 };
 
 export default EditTodoForm;
--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
-const TodoForm = () => {
-  <div>TodoForm</div>;
+// eslint-disable-next-line react/prop-types
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value) {
+      // add todo
+      addTodo(value);
+      // clear form after submission
+      setValue('');
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} className="TodoForm">
+      <Form.Group className="mb-3">
+        <input type="text" value={value} className="todo-input" placeholder="What is the task today?" onChange={(e) => setValue(e.target.value)} />
+        <Button variant="primary" type="submit" className="todo-btn">Add task</Button>
+      </Form.Group>
+    </Form>
+  );
 };
 
 export default TodoForm;
